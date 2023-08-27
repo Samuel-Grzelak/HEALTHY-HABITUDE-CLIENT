3c3f46521768c6e3cdb3db54cd84f0f9c7d4512a
Sam Grzelak <64333101+Samuel-Grzelak@users.noreply.github.com>
2023-08-27 11:13:31 -0500

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Samuel Grzelak | <b>Capstone 2023</b>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
